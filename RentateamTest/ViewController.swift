7e13b9e2e6489aec0b69692b6e7a1f08df2a6e98
Kazim Gajiev <mr.gadjiev2011@yandex.ru>
2019-08-27 09:20:15 +0300

    init commit

---
+++ RentateamTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RentateamTest
+//
+//  Created by Kazim Gajiev on 27/08/2019.
+//  Copyright © 2019 Kazim Gajiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
